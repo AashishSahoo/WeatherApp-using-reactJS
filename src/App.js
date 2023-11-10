0a6edf60d09a54f6c391c2fe86ae74e5abbf6665
ashish sahoo <ashishsahoo0013@gmail.com>
2023-11-11 00:04:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Temp from './components/weather/temp';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Temp /> 
+    </div>
+  );
+}
+
+export default App;
